c36a608e5abf49360c49c948cee74df4094672fd
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-07-12 12:36:15 -0300

    Exercício de Polimorfismo: implementando programa

--- src/application/ProgramEmployee.java
+++ src/application/ProgramEmployee.java
@@ -12,6 +12,41 @@ public class ProgramEmployee {
 
     public static void main(String[] args) {
 
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> emp = new ArrayList<>();
+
+        System.out.print("Digite o número de funcionários: ");
+        int func = sc.nextInt();
+
+        for (int i = 0; i < func; i++) {
+            System.out.println("Dados do Funcionário #" + (i + 1) + ": ");
+            System.out.print("Terceirizado? (s/n): ");
+            char terc = sc.next().charAt(0);
+            System.out.print("Nome: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Horas: ");
+            int hours = sc.nextInt();
+            System.out.print("Valor por hora: ");
+            double valuePerHour = sc.nextDouble();
+            if (terc == 's') {
+                System.out.print("Custo additional: ");
+                double additionalCharge = sc.nextDouble();
+                Employee employee = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+                emp.add(employee);
+            } else {
+                Employee employee = new Employee(name, hours, valuePerHour);
+                emp.add(employee);
+            }
+        }
+
+        System.out.println();
+        System.out.println("Pagamentos: ");
+        for (Employee em : emp) {
+            System.out.println(em);
+        }
+        sc.close();;
     }
 }
