2aacb175e210ca5fe7e578189c4099474c5bf96d
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-07-12 12:33:38 -0300

    Exercício de Polimorfismo: implmentando classes de entidades

---
+++ src/application/ProgramEmployee.java
@@ -0,0 +1,17 @@
+package application;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramEmployee {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
