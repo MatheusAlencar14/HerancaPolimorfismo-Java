7c1056a2cbf5bef88ed82385f4c95459077d366a
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-07-15 11:34:51 -0300

    Exercício Abstratas: implementando programa

--- src/application/ProgramShapeAbstratas.java
+++ src/application/ProgramShapeAbstratas.java
@@ -12,5 +12,42 @@ import java.util.Scanner;
 
 public class ProgramShapeAbstratas {
 
+    public static void main(String[] args) {
 
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Shape> shapes = new ArrayList<>();
+
+        System.out.print("Digite o número de figuras: ");
+        int fig = sc.nextInt();
+
+        for (int i = 0; i < fig; i++) {
+            System.out.println("Dados da " + (i+1) + "ª figura: ");
+            System.out.print("Retângulo ou Cículo? ");
+            char tipo = sc.next().charAt(0);
+            System.out.print("Cor: ");
+            Color color = Color.valueOf(sc.next());
+            if (tipo == 'r') {
+                System.out.print("Largura: ");
+                double width = sc.nextDouble();
+                System.out.print("ALtura: ");
+                double height = sc.nextDouble();
+                shapes.add(new Rectangle(color, width, height));
+            } else {
+                System.out.print("Raio: ");
+                double radius = sc.nextDouble();
+                shapes.add(new Circle(color, radius));
+            }
+        }
+
+        System.out.println();
+        System.out.println("Áreas das figuras: ");
+        for (Shape sh : shapes) {
+            System.out.println(String.format("%.2f", sh.area()));
+        }
+
+        sc.close();
+    }
 }
